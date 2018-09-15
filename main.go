bba1ca720df4b13ac48361862e8a06b8a724af3a
nickhstr <nickhstr@gmail.com>
2018-09-15 14:11:18 -0400

    feat: use new env utils

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/joho/godotenv"
 	"github.com/nickhstr/go-web-service/routes"
+	"github.com/nickhstr/go-web-service/utils/env"
 )
 
 func main() {
+	if env.IsDevEnv() {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
+	PORT := env.GetPort()
 	router := routes.NewRouter()
 
-	// Use localhost for dev
-	log.Fatal(http.ListenAndServe("localhost:3000", router))
+	init := func() error {
+		fmt.Printf("Listening on port: %s\n", PORT)
+		return http.ListenAndServe(PORT, router)
+	}
+
+	log.Fatal(init())
 }
