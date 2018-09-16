59fca5abb73bc2da9c12bfac623d516d61df750c
nickhstr <nickhstr@gmail.com>
2018-09-16 00:33:58 -0400

    chore: update comments

--- utils/env/env.go
+++ utils/env/env.go
@@ -6,7 +6,7 @@ import (
 )
 
 // GetGoEnv returns the GO_ENV value, if set.
-// Otherwise, it returns the default value
+// Otherwise, it returns the default value.
 func GetGoEnv() string {
 	if goEnv, isSet := os.LookupEnv("GO_ENV"); isSet {
 		return goEnv
@@ -15,17 +15,17 @@ func GetGoEnv() string {
 	return "development"
 }
 
-// IsDev indicates if app is in dev env
+// IsDev indicates if app is in dev env.
 func IsDev() bool {
 	return GetGoEnv() == "development"
 }
 
-// IsProd indicates if app is in prod env
+// IsProd indicates if app is in prod env.
 func isProd() bool {
 	return GetGoEnv() == "production"
 }
 
-// GetPort returns an appropriate port for http.ListenAndServe to use
+// GetPort returns an appropriate port for http.ListenAndServe to use.
 func GetPort() string {
 	defaultPort := "3000"
 	isDev := IsDev()
