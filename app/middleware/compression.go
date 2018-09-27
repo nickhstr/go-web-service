c9b5609d07c498623307be813e2b8e8fb9924fce
nickhstr <nickhstr@gmail.com>
2018-09-27 16:39:12 -0400

    fix: uncomment code

--- app/middleware/compression.go
+++ app/middleware/compression.go
@@ -4,14 +4,15 @@ import (
 	"net/http"
 
 	"github.com/gorilla/handlers"
+	"github.com/nickhstr/go-web-service/app/utils/env"
 )
 
 // Compression middleware compresses all handler outputs
 func Compression(handler http.Handler) http.Handler {
-	// if env.Get("COMPRESSION") == "true" {
-	// 	// Add gorilla compression middleware
-	// 	handler = handlers.CompressHandler(handler)
-	// }
+	if env.Get("COMPRESSION") == "true" {
+		// Add gorilla compression middleware
+		handler = handlers.CompressHandler(handler)
+	}
 
 	handler = handlers.CompressHandler(handler)
 
