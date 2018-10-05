8372d97bb7840526efb3d361dcaef6a836b0d378
nickhstr <nickhstr@gmail.com>
2018-10-05 17:00:18 -0400

    fix: don't exit on failed .env load

--- app/app.go
+++ app/app.go
@@ -14,7 +14,7 @@ import (
 func Init() {
 	if env.IsDev() {
 		if err := godotenv.Load(); err != nil {
-			log.Fatal("Error loading .env file")
+			log.Println("Could not load .env file")
 		}
 	}
 
