6308e9d1f3a55677edd43ae431f82c4a48a9eae2
nickhstr <nickhstr@gmail.com>
2019-03-17 19:26:00 -0400

    chore: update go version

--- go.mod
+++ go.mod
@@ -1,10 +1,12 @@
 module github.com/nickhstr/go-web-service
 
+go 1.12
+
 require (
 	github.com/go-chi/chi v3.3.3+incompatible
 	github.com/nickhstr/goweb v0.2.2
 	golang.org/x/net v0.0.0-20181207154023-610586996380 // indirect
 )
 
-// +heroku goVersion go1.11
+// +heroku goVersion go1.12
 // +heroku install .
--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	_ "github.com/nickhstr/goweb/logger"
-	"github.com/nickhstr/goweb/env"
-	"github.com/nickhstr/goweb/middleware"
-	"github.com/nickhstr/goweb/server" // nolint: gotype
 	"github.com/nickhstr/go-web-service/routes"
+	"github.com/nickhstr/goweb/env"
+	_ "github.com/nickhstr/goweb/logger"
+	"github.com/nickhstr/goweb/middleware"
+	"github.com/nickhstr/goweb/server"
 )
 
 func main() {
 	mux := middleware.Create(middleware.Config{
-		AppName: env.AppName(),
-		AppVersion: env.Get("APP_VERSION"),
+		AppName:     env.AppName(),
+		AppVersion:  env.Get("APP_VERSION"),
 		GitRevision: env.Get("GIT_COMMIT"),
-		Handler: routes.Router,
-		Region: env.Get("REGION"),
+		Handler:     routes.Router,
+		Region:      env.Get("REGION"),
 	})
 
 	server.Start(mux)
