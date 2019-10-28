d00478c6c55bb8b59a6d995d5d9202dfbe883407
nickhstr <nickhstr@gmail.com>
2019-10-28 18:09:57 -0400

    refactor: remove unnecessary import

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/nickhstr/go-web-service/routes"
 	"github.com/nickhstr/goweb/env"
-	_ "github.com/nickhstr/goweb/logger"
 	"github.com/nickhstr/goweb/middleware"
 	"github.com/nickhstr/goweb/server"
 )
