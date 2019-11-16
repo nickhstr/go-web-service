b961d53f32fa7fade24c3f38472d3426c8ccc8d4
nickhstr <nickhstr@gmail.com>
2019-11-16 15:08:10 -0500

    feat: log and exit on server.Start error

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/nickhstr/go-web-service/routes"
 	"github.com/nickhstr/goweb/env"
+	"github.com/nickhstr/goweb/logger"
 	"github.com/nickhstr/goweb/middleware"
 	"github.com/nickhstr/goweb/server"
 	"github.com/rs/cors"
@@ -12,6 +13,8 @@ var gitCommit = "<not set>"
 var appVersion = "<not set>"
 
 func main() {
+	log := logger.New("go-web-service")
+
 	handler := middleware.Compose(
 		routes.Router,
 		// Add additional middleware here
@@ -29,5 +32,7 @@ func main() {
 		Region:      env.Get("REGION"),
 	})
 
-	server.Start(mux)
+	if err := server.Start(mux); err != nil {
+		log.Fatal().Err(err).Msg("failed to start server")
+	}
 }
